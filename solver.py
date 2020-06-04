b1ba51e13a4c79be4018931ad8896332c26bf091
Nicolas Appel <n.appel@tum.de>
2020-06-04 20:44:30 +0200

    Improved structural evaluation using numpy

--- solver.py
+++ solver.py
@@ -1,39 +1,87 @@
 import time
 import math as m
+import numpy as np
 from collections import Counter
 import copy
 
 def ip(c): #invert player
-    if c == 'w': 
-        return 'b'
-    elif c == 'b': 
-        return 'w'
+    if c == -1: 
+        return 1
+    elif c == 1: 
+        return -1
     else: 
         return False
-        
+
+def format_player(c):
+    if c == -1:
+        return '◆'
+    elif c == 1:
+        return '◇'
+    else:
+        return '▢'
+
+class Move:
+    def __init__(self,x,y):
+        self.x = int(x)
+        self.y = int(y)
+
+    def __str__(self):
+        return f"{chr(self.x+97)}{8-self.y}"
+    
+    def __eq__(self, other): 
+        if not isinstance(other, Move):
+            return NotImplemented
+        else:
+            if other.x == self.x and other.y == self.y:
+                return True
+            else:
+                return False
+    def __lt__(self,other):
+        if not isinstance(other, Move):
+            return NotImplemented
+        else:
+            return (self.x,self.y)<=(other.x,other.y)
+
+    def isValid(self):
+        if self.x >= 0 and self.x <= 7 and self.y >= 0 and self.y <= 7:
+            return True
+        else:
+            return False
+    def __hash__(self):
+        return self.x*16+self.y
+ 
+
+
 
 class Game:
     def __init__(self):
         self.initialize_game()
     
     def initialize_game(self):
-        self.game_state = [['.','.','.','.','.','.','.','.'],
-                            ['.','.','.','.','.','.','.','.'],
-                            ['.','.','.','.','.','w','.','.'],
-                            ['.','.','.','b','w','b','.','.'],
-                            ['.','.','b','w','b','.','.','.'],
-                            ['.','.','w','.','.','.','.','.'],
-                            ['.','.','.','.','.','.','.','.'],
-                            ['.','.','.','.','.','.','.','.']]
-        self.structure = [[16, -4, 4, 2, 2, 4, -4, 16],
+        self.game_state = np.zeros((8,8))
+        self.game_state[3][3] = self.game_state[4][4] = -1
+        self.game_state[3][4] = self.game_state[4][3] = 1
+
+        self.structure = np.array([[16, -4, 4, 2, 2, 4, -4, 16],
                             [-4, 12, -2, -2 ,-2, -2, -12,-4],
                             [4, -2, 4, 2, 2, 4, -2, 4],
                             [2, -2, 2, 0, 0, 2, -2, 2],
                             [2, -2, 2, 0, 0, 2, -2, 2],
                             [4, -2, 4, 2, 2, 4, -2, 4],
                             [-4, 12, -2, -2 ,-2, -2, -12,-4],
-                            [16, -4, 4, 2, 2, 4, -4, 16]]
-        self.depth = 4
+                            [16, -4, 4, 2, 2, 4, -4, 16]])
+        self.depth = 6
+        self.counter = self.count()
+        self.pruned = 0
+        self.evals = 0
+        self.moves = 0
+        
+
+    #TODO: Implement actual test
+    def move_valid(self, move, player):
+        if self.game_state[move.y][move.x] != 0:
+            return False
+        return True
 
     def count(self):
         c = Counter(self.game_state[0])
@@ -46,25 +94,26 @@ class Game:
         for i in range(0,8):
             print(8-i, end=" ")
             for j in range(0,8):
-                print('{}'.format(self.game_state[i][j]), end=" ")
+                print('{}'.format(format_player(self.game_state[i][j])), end=" ")
             print()
         print("  A B C D E F G H")
 
     def test_moves(self, i, j, player):
         retval = []
-        for xinc in range(-1,2):
-            for yinc in range(-1,2):
+        for yinc in range(-1,2):
+            for xinc in range(-1,2):
                 if xinc == 0 and yinc == 0:
                     continue
-                x = i
-                y = j
+                x = j
+                y = i
+
                 nmoves = 0
 
                 while True:
                     x += xinc
                     y += yinc
                     if x>=0 and y>=0 and x<=7 and y<=7:
-                        if self.game_state[x][y] == ip(player):
+                        if self.game_state[y][x] == ip(player):
                             nmoves += 1
                         else:
                             break
@@ -72,10 +121,11 @@ class Game:
                         nmoves = 0
                         break
 
-                if nmoves >  0 and self.game_state[x][y] == '.':
+                if nmoves >  0 and self.game_state[y][x] == 0:
                     if x < 0 or y < 0:
                         print("boink")
-                    retval.append((x,y))
+                    else:
+                        retval.append(Move(x,y))
 
         return retval
     
@@ -92,85 +142,100 @@ class Game:
 
     def make_move(self,player,move):
         flipstack = []
-        i = move[0]
-        j = move[1]
+        i = move.y
+        j = move.x
 
         self.game_state[i][j] = player
 
-        for xinc in range(-1,2):
-            for yinc in range(-1,2):
+        for yinc in range(-1,2):
+            for xinc in range(-1,2):
                 if xinc == 0 and yinc ==0:
                     continue
-                x = i+xinc
-                y = j+yinc
+                y = i
+                x = j
                 nmoves = 0
 
-                try:
-                    while self.game_state[x][y] == ip(player):
-                        x += xinc
-                        y += yinc
-                        nmoves += 1
+                while True:
+                    x += xinc
+                    y += yinc
+                    if x>=0 and y>=0 and x<=7 and y<=7:
+                        if self.game_state[y][x] == ip(player):
+                            nmoves += 1
+                        else:
+                            break
+                    else:
+                        nmoves = 0
+                        break
 
-                    if nmoves > 0 and self.game_state[x][y] == player:
+                if nmoves > 0 and self.game_state[y][x] == player:
+                    x -= xinc
+                    y -= yinc
+                    while self.game_state[y][x] == ip(player):
+                        flipstack.append(Move(x,y))
+                        self.game_state[y][x] = player                      
                         x -= xinc
                         y -= yinc
-                        while self.game_state[x][y] == ip(player):
-                            flipstack.append((x,y))
-                            self.game_state[x][y] = player
-                            x -= xinc
-                            y -= yinc
-                except:
-                    continue
+        self.counter[player] += len(flipstack) + 1
+        self.counter[ip(player)] -= len(flipstack)
+
+        self.moves += 1
         return flipstack
     
     def undo_move(self,move,flipstack):
-        i = move[0]
-        j = move[1]
+        i = move.y
+        j = move.x
 
-        self.game_state[i][j] = '.'
+        player = self.game_state[i][j]
+        self.counter[player] -= len(flipstack) - 1
+        self.counter[ip(player)] += len(flipstack)
+        self.game_state[i][j] = 0
 
         for f in flipstack:
-            self.game_state[f[0]][f[1]] = ip(self.game_state[f[0]][f[1]])
+            self.game_state[f.y][f.x] = ip(player)
 
+    def outside_square(self):
+
+        #Trivial case
+        if sum(self.counter.values()) > 16:
+            return True
+        
+        #search square around inner 4x4
+        for i in [1,6]:
+            for j in range(1,6):
+                if self.game_state[i][j] != 0:
+                    return True
+
+        for j in [1, 6]:
+            for i in range(2,5):
+                if self.game_state[i][j] != 0:
+                    return True
+        #Nothing
+        return False
     
     def eval_structure(self,c):
-        sum = 0
-        outside_square = False
-       
-        for i in range(0,8):
-            for j in range(0,8):
-                if not(i > 1 and i < 6 and j > 1 and j < 6) and self.game_state[i][j] != '.':
-                    outside_square == True
-                    break
-            if outside_square == True:
-                break
+        structure_sum = 0
+        outside_square = self.outside_square()
 
-        for i in range(0,8):
-            for j in range(0,8):
-                if outside_square == True and i > 1 and i < 6 and j > 1 and j < 6:
-                    continue
-                elif self.game_state[i][j] == '.':
-                    continue
-                elif self.game_state[i][j] == c:
-                    sum += self.structure[i][j]
-                else:
-                    sum -= self.structure[i][j]
-        if outside_square == True:
-            sum *= 3
-        return sum
+        structure_sum = sum(sum(np.matmul(self.game_state,self.structure)))
+
+        #assume outside square
+        if outside_square == False:
+            diff = sum(sum(np.matmul(self.game_state[2:6][2:6],self.structure[2:6][2:6])))
+            structure_sum -= diff
+        else:
+            structure_sum *= 3
+        return structure_sum
 
     def eval(self,player,moves):
-        c = self.count()
-        omoves = self.next_moves(ip(c))
+        self.evals += 1
+
+        omoves = self.next_moves(ip(player))
         M = (len(moves)-len(omoves))*2
         S = self.eval_structure(player)
 
-        if player == 'w':
-            A = c['w'] - c['b']
-        else:
-            A = c['b'] - c['w']
-        
-        n = c['w'] + c['b']
+        A = self.counter[player] - self.counter[ip(player)]
+     
+        n = sum(self.counter.values())
         z = 0.035
         W = m.exp(-n*z)
 
@@ -200,81 +265,101 @@ class Game:
                 value = min(value, self.minimax(ip(c),maxi,depth-1))
                 self.undo_move(mov,s)
             return value
-    
-    def alphabeta(self, c, maxi, depth, alpha, beta):
-        moves = self.next_moves(c)
-        retmv = None
-        
-        #game is finished can't move anymore
-        if len(moves) == 0 or depth == 0:
-            return (self.eval(c,moves),retmv)
-        
-        if maxi == True:
-            max_value = -m.inf
-            for mov in moves:
-                s = self.make_move(c,mov)
-                (value,xxx) = self.alphabeta(ip(c),False,depth-1,max_value,beta)
-                self.undo_move(mov,s)
-                if value > max_value:
-                    max_value = value
-                    if depth == self.depth:
-                        retmv = mov
-                    if max_value >= beta:
-                        break
-            return (value,retmv)
-        else:
-            min_value = m.inf
-            for mov in moves:
-                s = self.make_move(c,mov)
-                (value,xxx) = self.alphabeta(ip(c),True,depth-1,alpha,min_value)
-                self.undo_move(mov,s)
-                if value < min_value:
-                    min_value = value
-                    if min_value <= alpha:
-                        break
-            return (value,None)
 
+    def alphabeta_max(self, c, depth, alpha, beta):
+        moves = self.next_moves(c)
+        if self.depth == depth and len(moves) == 1:
+            return (m.inf, moves[0])
+
+        if len(moves) == 0 or depth == 0:
+            return (self.eval(c,moves),None)
+        
+        retmv = None      
+        max_value = alpha
+        for mov in moves:
+            s = self.make_move(c,mov)
+            value = self.alphabeta_min(ip(c),depth-1,max_value,beta)
+            self.undo_move(mov,s)
+            if value > max_value:
+                max_value = value
+                if depth == self.depth:
+                    retmv = mov
+                if max_value >= beta:
+                    self.pruned += 1
+                    break
+        return (max_value,retmv)
+    
+    def alphabeta_min(self, c, depth, alpha, beta):
+        moves = self.next_moves(c)
+        if len(moves) == 0 or depth == 0:
+            return self.eval(c,moves)
+
+        min_value = beta
+        for mov in moves:
+            s = self.make_move(c,mov)
+            (value,xxx) = self.alphabeta_max(ip(c),depth-1,alpha,min_value)
+            self.undo_move(mov,s)
+            if value < min_value:
+                min_value = value
+                if min_value <= alpha:
+                    self.pruned += 1
+                    break
+        return min_value
+    
 def main():
     g = Game()
     g.draw_board()
 
     passing = 0
-    cp = 'b'
+    cp = -1
     moves = []
 
-    g.depth = 10
+    g.depth = 8
     passing = 0
 
-    for i in range(0,12):
-        try:
-            
-            (value,mv) = g.alphabeta(cp,True,g.depth,m.inf,-m.inf)
+    pm = None    
 
-            if mv != None:
-                g.make_move(cp,mv)
-                moves.append(mv)
-                g.draw_board()
-                passing = 0
-            else:
-                passing += 1
+    while True:
+        if cp == -1:
+            t1 = time.time()
+            (value,mv) = g.alphabeta_max(cp,g.depth,-m.inf,m.inf)
+            t1 = time.time() - t1
+            print(t1)
+            print("Evals: {} Moves: {} Pruned: {}".format(g.evals,g.moves,g.pruned))
+        else:
+            try:
+                while True:
+                    (x,y) = input("Move: ").split()
+                    x = ord(x) - 97
+                    y = 8-int(y)
+                    mv = Move(x,y)
+                    if g.move_valid(mv, cp):
+                        break
+            except:
+                mv = None
 
-            if passing == 2:
-                break
+        if mv != None:
+            g.make_move(cp,mv)
+            moves.append(mv)
+            g.draw_board()
+            print(mv)
+            passing = 0
+        else:
+            passing += 1
 
-            cp = ip(cp)
-        except KeyboardInterrupt:
+        if passing == 2:
             break
+
+        cp = ip(cp)
     
     c = g.count()
-    if c['w'] > c['b']:
+    if c[-1] > c[1]:
         print("White won!")
     else:
         print("Black won!")
 
     for mov in moves:
-        letter = chr(mov[1]+97)
-        number = 8-mov[0]
-        print("{}{}".format(letter,number),end="")
+        print(mov,end="")
     print()
 
 if __name__ == "__main__":
