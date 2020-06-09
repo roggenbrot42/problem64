3ae71ab1d1d5ff358cedc83ba87a9722c8653433
Nicolas Appel <n.appel@tum.de>
2020-06-09 09:48:28 +0200

    Changes: - Rewrote flipping logic to use a preallocated stack (don't know if this   increased performance, but I hope it does in C) - Fixed bugs in alphabeta and eval functions - Moved Players to class, changed colors to int - Better board representation when printing - Structure evaluation now assumes outside_square to be true - Introduced some benchmarking variables for evaluations, pruned   branches, and moves made

--- solver.py
+++ solver.py
@@ -4,21 +4,22 @@ import numpy as np
 from collections import Counter
 import copy
 
-def ip(c): #invert player
-    if c == -1: 
-        return 1
-    elif c == 1: 
-        return -1
-    else: 
-        return False
-
-def format_player(c):
-    if c == -1:
-        return '◆'
-    elif c == 1:
-        return '◇'
-    else:
-        return '▢'
+class Color:
+    @staticmethod
+    def ip(c): #invert color
+        return -c
+    
+    none = 0
+    white = -1
+    black = 1
+    @staticmethod
+    def format_player(c):
+        if c == -1:
+            return '◆'
+        elif c == 1:
+            return '◇'
+        else:
+            return '▢'
 
 class Move:
     def __init__(self,x,y):
@@ -38,7 +39,7 @@ class Move:
                 return False
     def __lt__(self,other):
         if not isinstance(other, Move):
-            return NotImplemented
+            return False
         else:
             return (self.x,self.y)<=(other.x,other.y)
 
@@ -50,6 +51,24 @@ class Move:
     def __hash__(self):
         return self.x*16+self.y
  
+class Stack:
+    def __init__(self, size):
+        self.stack = np.zeros(size,dtype=type((0,0)))
+        self.sp = -1
+        self.size = size
+    
+    def push(self, y, x):
+        if self.sp < self.size-1:
+            self.sp += 1
+            self.stack[self.sp] = (y,x)
+    
+    def pop(self):
+        retval = None
+        if self.sp >= 0 and self.sp < self.size:
+            retval = self.stack[self.sp]
+            self.sp -= 1
+        return retval
+    
 
 
 
@@ -59,10 +78,18 @@ class Game:
     
     def initialize_game(self):
         self.game_state = np.zeros((8,8))
-        self.game_state[3][3] = self.game_state[4][4] = -1
-        self.game_state[3][4] = self.game_state[4][3] = 1
+        self.game_state[3][3] = self.game_state[4][4] = Color.black
+        self.game_state[3][4] = self.game_state[4][3] = Color.white
 
-        self.structure = np.array([[16, -4, 4, 2, 2, 4, -4, 16],
+        self.depth = 12
+        self.counter = self.count()
+        self.pruned = 0
+        self.evals = 0
+        self.moves = 0
+        self.current_player = Color.black
+        self.flipstack = Stack(3000)
+    
+    structure = np.array([[16, -4, 4, 2, 2, 4, -4, 16],
                             [-4, 12, -2, -2 ,-2, -2, -12,-4],
                             [4, -2, 4, 2, 2, 4, -2, 4],
                             [2, -2, 2, 0, 0, 2, -2, 2],
@@ -70,16 +97,11 @@ class Game:
                             [4, -2, 4, 2, 2, 4, -2, 4],
                             [-4, 12, -2, -2 ,-2, -2, -12,-4],
                             [16, -4, 4, 2, 2, 4, -4, 16]])
-        self.depth = 6
-        self.counter = self.count()
-        self.pruned = 0
-        self.evals = 0
-        self.moves = 0
         
 
     #TODO: Implement actual test
     def move_valid(self, move, player):
-        if self.game_state[move.y][move.x] != 0:
+        if self.game_state[move.y][move.x] != Color.none:
             return False
         return True
 
@@ -94,11 +116,11 @@ class Game:
         for i in range(0,8):
             print(8-i, end=" ")
             for j in range(0,8):
-                print('{}'.format(format_player(self.game_state[i][j])), end=" ")
+                print('{}'.format(Color.format_player(self.game_state[i][j])), end=" ")
             print()
         print("  A B C D E F G H")
 
-    def test_moves(self, i, j, player):
+    def test_move(self, i, j, player):
         retval = []
         for yinc in range(-1,2):
             for xinc in range(-1,2):
@@ -113,7 +135,7 @@ class Game:
                     x += xinc
                     y += yinc
                     if x>=0 and y>=0 and x<=7 and y<=7:
-                        if self.game_state[y][x] == ip(player):
+                        if self.game_state[y][x] == Color.ip(player):
                             nmoves += 1
                         else:
                             break
@@ -121,7 +143,7 @@ class Game:
                         nmoves = 0
                         break
 
-                if nmoves >  0 and self.game_state[y][x] == 0:
+                if nmoves >  0 and self.game_state[y][x] == Color.none:
                     if x < 0 or y < 0:
                         print("boink")
                     else:
@@ -129,19 +151,28 @@ class Game:
 
         return retval
     
+    #TODO: this function is too slow
     def next_moves(self,player):
         next_moves = []
-        for i in range(0,8):
-            for j in range(0,8):
+
+        if self.outside_square() == False:
+            rmin = 1
+            rmax = 7
+        else:
+            rmin = 0
+            rmax = 8
+
+        for i in range(rmin,rmax):
+            for j in range(rmin,rmax):
                 if(self.game_state[i][j] == player):
-                    m = self.test_moves(i,j,player)
+                    m = self.test_move(i,j,player)
                     next_moves += m
         used = set()
         unique = [x for x in next_moves if x not in used and (used.add(x) or True)]
         return unique
 
     def make_move(self,player,move):
-        flipstack = []
+        flipped = 0
         i = move.y
         j = move.x
 
@@ -159,7 +190,7 @@ class Game:
                     x += xinc
                     y += yinc
                     if x>=0 and y>=0 and x<=7 and y<=7:
-                        if self.game_state[y][x] == ip(player):
+                        if self.game_state[y][x] == Color.ip(player):
                             nmoves += 1
                         else:
                             break
@@ -170,44 +201,45 @@ class Game:
                 if nmoves > 0 and self.game_state[y][x] == player:
                     x -= xinc
                     y -= yinc
-                    while self.game_state[y][x] == ip(player):
-                        flipstack.append(Move(x,y))
+                    while self.game_state[y][x] == Color.ip(player):
+                        self.flipstack.push(y,x)
+                        flipped += 1
                         self.game_state[y][x] = player                      
                         x -= xinc
                         y -= yinc
-        self.counter[player] += len(flipstack) + 1
-        self.counter[ip(player)] -= len(flipstack)
+        self.counter[player] += flipped + 1
+        self.counter[Color.ip(player)] -= flipped
 
         self.moves += 1
-        return flipstack
+        return flipped
     
-    def undo_move(self,move,flipstack):
+    def undo_move(self,move,flipped):
         i = move.y
         j = move.x
 
         player = self.game_state[i][j]
-        self.counter[player] -= len(flipstack) - 1
-        self.counter[ip(player)] += len(flipstack)
-        self.game_state[i][j] = 0
+        self.counter[player] -=  flipped + 1
+        self.counter[Color.ip(player)] += flipped
+        self.game_state[i][j] = Color.none
 
-        for f in flipstack:
-            self.game_state[f.y][f.x] = ip(player)
+        for i in range(0,flipped):
+            (y,x) = self.flipstack.pop()
+            self.game_state[y][x] = Color.ip(player)
 
     def outside_square(self):
-
         #Trivial case
-        if sum(self.counter.values()) > 16:
+        if self.counter[Color.none] >= 48:
             return True
         
         #search square around inner 4x4
         for i in [1,6]:
             for j in range(1,6):
-                if self.game_state[i][j] != 0:
+                if self.game_state[i][j] != Color.none:
                     return True
 
         for j in [1, 6]:
             for i in range(2,5):
-                if self.game_state[i][j] != 0:
+                if self.game_state[i][j] != Color.none:
                     return True
         #Nothing
         return False
@@ -224,18 +256,19 @@ class Game:
             structure_sum -= diff
         else:
             structure_sum *= 3
-        return structure_sum
+        return c*structure_sum
 
-    def eval(self,player,moves):
+    def eval(self):
         self.evals += 1
 
-        omoves = self.next_moves(ip(player))
-        M = (len(moves)-len(omoves))*2
-        S = self.eval_structure(player)
+        omoves = self.next_moves(-self.current_player)
+        moves = self.next_moves(self.current_player)
 
-        A = self.counter[player] - self.counter[ip(player)]
+        M = (len(moves)-len(omoves))*2
+        S = self.eval_structure(self.current_player)
+        A = self.counter[self.current_player] - self.counter[-self.current_player]
      
-        n = sum(self.counter.values())
+        n = self.counter[Color.white]+self.counter[Color.black]
         z = 0.035
         W = m.exp(-n*z)
 
@@ -243,61 +276,77 @@ class Game:
         return score
 
     
-    def minimax(self, c, maxi, depth):
+    def minimax_max(self, c, depth):
+        moves = self.next_moves(c)
+
+        if self.depth == depth and len(moves) == 1:
+            return (m.inf, moves[0])
+
+        #game is finished can't move anymore
+        if len(moves) == 0 or depth == 0:
+            return (self.eval(c,moves),None)
+
+        retmv = None
+
+        max_value = -m.inf
+        for mov in moves:
+            s = self.make_move(c,mov)
+            value = self.minimax_min(Color.ip(c),depth-1)
+            max_value = max(max_value,value)
+            self.undo_move(mov,s)
+        return (value,retmv)
+
+    def minimax_min(self, c, depth):
         moves = self.next_moves(c)
 
         #game is finished can't move anymore
         if len(moves) == 0 or depth == 0:
             return self.eval(c,moves)
-        
 
-        if maxi == True:
-            value = -m.inf
-            for mov in moves:
-                s = self.make_move(c,mov)
-                value = max(value,self.minimax(ip(c),not(maxi),depth-1))
-                self.undo_move(mov,s)
-            return value
-        else:
-            value = m.inf
-            for mov in moves:
-                s = self.make_move(c,mov)
-                value = min(value, self.minimax(ip(c),maxi,depth-1))
-                self.undo_move(mov,s)
-            return value
+        min_value = m.inf
+        for mov in moves:
+            s = self.make_move(c,mov)
+            (value,xxx) = self.minimax_max(Color.ip(c),depth-1)
+            min_value = min(min_value, value)
+            self.undo_move(mov,s)
+        return value
 
-    def alphabeta_max(self, c, depth, alpha, beta):
-        moves = self.next_moves(c)
+    def alphabeta_max(self, depth, alpha, beta):
+        moves = self.next_moves(self.current_player)
         if self.depth == depth and len(moves) == 1:
             return (m.inf, moves[0])
 
         if len(moves) == 0 or depth == 0:
-            return (self.eval(c,moves),None)
+            return (self.eval(),None)
         
-        retmv = None      
+        retmov = None      
         max_value = alpha
         for mov in moves:
-            s = self.make_move(c,mov)
-            value = self.alphabeta_min(ip(c),depth-1,max_value,beta)
+            s = self.make_move(self.current_player,mov)
+            value = self.alphabeta_min(depth-1,max_value,beta)
             self.undo_move(mov,s)
             if value > max_value:
                 max_value = value
                 if depth == self.depth:
-                    retmv = mov
+                    retmov = mov
                 if max_value >= beta:
                     self.pruned += 1
                     break
-        return (max_value,retmv)
+            #samesies but use the alphabetically lower version
+            if value == max_value and mov and mov < retmov:
+                retmov = mov                
+
+        return (max_value,retmov)
     
-    def alphabeta_min(self, c, depth, alpha, beta):
-        moves = self.next_moves(c)
+    def alphabeta_min(self, depth, alpha, beta):
+        moves = self.next_moves(Color.ip(self.current_player))
         if len(moves) == 0 or depth == 0:
-            return self.eval(c,moves)
+            return self.eval()
 
         min_value = beta
         for mov in moves:
-            s = self.make_move(c,mov)
-            (value,xxx) = self.alphabeta_max(ip(c),depth-1,alpha,min_value)
+            s = self.make_move(-self.current_player,mov)
+            (value,xxx) = self.alphabeta_max(depth-1,alpha,min_value)
             self.undo_move(mov,s)
             if value < min_value:
                 min_value = value
@@ -308,52 +357,69 @@ class Game:
     
 def main():
     g = Game()
-    g.draw_board()
 
     passing = 0
-    cp = -1
     moves = []
 
-    g.depth = 8
+    g.depth = 7
     passing = 0
 
-    pm = None    
+    pm = None
+
+    g.game_state[2][5] = Color.white
+    g.game_state[3][5] = Color.black
+    g.game_state[4][2] = Color.black
+    g.game_state[5][2] = Color.white
+
+    g.draw_board()
+
+
+    g.counter = g.count()
+
 
     while True:
-        if cp == -1:
-            t1 = time.time()
-            (value,mv) = g.alphabeta_max(cp,g.depth,-m.inf,m.inf)
-            t1 = time.time() - t1
-            print(t1)
-            print("Evals: {} Moves: {} Pruned: {}".format(g.evals,g.moves,g.pruned))
-        else:
-            try:
-                while True:
-                    (x,y) = input("Move: ").split()
-                    x = ord(x) - 97
-                    y = 8-int(y)
-                    mv = Move(x,y)
-                    if g.move_valid(mv, cp):
-                        break
-            except:
-                mv = None
+        #if cp == Color.black:
+        cev = g.evals
+        cmv = g.moves
+        cpr = g.pruned
+        t1 = time.time()
+        (value,mv) = g.alphabeta_max(g.depth,-m.inf,m.inf)
+        #(value,mv) = g.minimax_max(cp,g.depth)
+        t1 = time.time() - t1
+        print("Time: {}s".format(t1))
+        cev = g.evals - cev
+        cmv = g.moves - cmv
+        cpr = g.pruned - cpr
+        print("Evals: {} Evals/s: {} Moves: {} Pruned: {}".format(cev,cev/t1,cmv,cpr))
+       # else:
+
+            #try:
+            #    while True:
+            #        (x,y) = input("Move: ").split()
+            #        x = ord(x) - 97
+            #        y = 8-int(y)
+            #        mv = Move(x,y)
+            #        if g.move_valid(mv, cp):
+            #            break
+            #except:
+            #    mv = None
 
         if mv != None:
-            g.make_move(cp,mv)
+            g.make_move(g.current_player,mv)
             moves.append(mv)
             g.draw_board()
-            print(mv)
+            print("Color: {} Move: {} Value: {:0.9f}".format(Color.format_player(g.current_player),mv,value))
             passing = 0
         else:
             passing += 1
-
+            print("Color: {}".format(Color.format_player(g.current_player)))
         if passing == 2:
             break
 
-        cp = ip(cp)
+        g.current_player = -g.current_player
     
     c = g.count()
-    if c[-1] > c[1]:
+    if c[Color.white] > c[Color.black]:
         print("White won!")
     else:
         print("Black won!")
