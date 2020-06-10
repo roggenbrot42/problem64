98733096ad7a1bab96f2c65111205986f0f7ae09
Nicolas Appel <n.appel@tum.de>
2020-06-10 22:37:20 +0200

    Switched board representation to 64-bit integers

--- solver.py
+++ solver.py
@@ -3,17 +3,19 @@ import math as m
 import numpy as np
 from collections import Counter
 import copy
+from bitarray import bitarray
+from bitarray import util as btutil
 
 class Color:
+    NONE = 0
+    WHITE = -1
+    BLACK = 1
+    def __neg__(self):
+        return -self.value
+    def __int__(self):
+        return self.value
     @staticmethod
-    def ip(c): #invert color
-        return -c
-    
-    none = 0
-    white = -1
-    black = 1
-    @staticmethod
-    def format_player(c):
+    def format(c): #requires dark mode terminal
         if c == -1:
             return '◆'
         elif c == 1:
@@ -27,7 +29,7 @@ class Move:
         self.y = int(y)
 
     def __str__(self):
-        return f"{chr(self.x+97)}{8-self.y}"
+        return f"{chr(104-self.x)}{self.y+1}"
     
     def __eq__(self, other): 
         if not isinstance(other, Move):
@@ -69,6 +71,127 @@ class Stack:
             self.sp -= 1
         return retval
     
+class Board:
+
+    wstate = 0
+    bstate = 0
+    square_mask = 0x00003C3C3C3C0000
+
+    def __init__(self,wstate,bstate):
+        self.wstate = wstate
+        self.bstate = bstate
+    
+    def copy(self):
+        board = Board(self.wstate,self.bstate)
+        return board
+    
+    def __getitem__(self, key):
+        if (self.wstate >> key) & 1:
+            return Color.WHITE
+        elif (self.bstate >> key) & 1:
+            return Color.BLACK
+        else:
+            return Color.NONE
+
+    def __setitem__(self,key,value):
+        if value == Color.WHITE:
+            self.wstate = self.wstate | (1 << key)
+        elif value == Color.BLACK:
+            self.bstate = self.bstate | (1 << key)
+        else:
+            self.wstate &= ~(1 << key)
+            self.bstate &= ~(1 << key)
+    
+    def flip(self,key):
+        self.wstate = self.wstate ^ (1 << key)
+        self.bstate = self.bstate ^ (1 << key)
+    
+    def delitem(self,key):
+        self.wstate &= ~(1 << key)
+        self.bstate &= ~(1 << key)
+
+    @staticmethod
+    def bit_reverse_8(input):
+        val = ((input >> 1) & 0x5555555555555555) | ((input << 1) & 0xAAAAAAAAAAAAAAAAAA)
+        val = ((val >> 2) & 0x3333333333333333) | ((val << 2) & 0xCCCCCCCCCCCCCCCC)
+        val = ((val >> 4) & 0x0F0F0F0F0F0F0F0F) | ((val << 4) & 0xF0F0F0F0F0F0F0F0)
+        return val
+
+    @staticmethod
+    def bit_reverse_64(input):
+        val = ((input >> 1) & 0x5555555555555555) | ((input << 1) & 0xAAAAAAAAAAAAAAAAAA)
+        val = ((val >> 2) & 0x3333333333333333) | ((val << 2) & 0xCCCCCCCCCCCCCCCC)
+        val = ((val >> 4) & 0x0F0F0F0F0F0F0F0F) | ((val << 4) & 0xF0F0F0F0F0F0F0F0)
+        val = ((val >> 8) & 0x00FF00FF00FF00FF) | ((val << 8) & 0xFF00FF00FF00FF00)
+        val = ((val >> 16)& 0x0000FFFF0000FFFF) | ((val << 16)& 0xFFFF0000FFFF0000)
+        val = ((val >> 32)& 0x00000000FFFFFFFF) | ((val << 32)& 0xFFFFFFFF00000000)
+        return val
+    
+    @staticmethod
+    def flip_vertically(input):
+        val = ((input & 0x00000000FFFFFFFF) << 32) | ((input & 0xFFFFFFFF00000000) >> 32)
+        val = ((val & 0x0000FFFF0000FFFF) << 16) | ((val & 0xFFFF0000FFFF0000) >> 16)
+        val = ((val & 0x00FF00FF00FF00FF) << 8) | ((val & 0xFF00FF00FF00FF00) >> 8)
+        return val
+    
+    def outside_square(self):
+        return (self.wstate | self.bstate) & self.square_mask > 0
+
+    def check_symmetries(self):
+        symmetry = 0
+        #check x symmetry:
+        a = self.bit_reverse_8(self.wstate)
+        b = self.bit_reverse_8(self.bstate)
+        if (a == self.wstate and b == self.bstate):
+            symmetry |= 1
+        #check symmetry along (00) to (88)
+        a = self.bit_reverse_64(a)
+        b = self.bit_reverse_64(b)
+        if (a == self.wstate and b == self.bstate):
+            symmetry |= 4
+        #check y symmetry:
+        a = self.flip_vertically(self.wstate)
+        b = self.flip_vertically(self.wstate)
+        if a == self.wstate and b == self.bstate:
+            symmetry |= 2
+        #check symmetry along (08) to (80)
+        a = self.bit_reverse_64(self.wstate)
+        b = self.bit_reverse_64(self.bstate)
+        if a == self.wstate and b == self.bstate:
+            symmetry |= 8
+        return symmetry
+    
+    def count(self):
+        a = btutil.int2ba(self.wstate)
+        b = btutil.int2ba(self.bstate)
+        c = Counter()
+        c[Color.WHITE] = a.count(True)
+        c[Color.BLACK] = b.count(True)
+        return c
+
+    @staticmethod
+    def tostring(x,y):
+        a = btutil.int2ba(x,length=64)
+        b = btutil.int2ba(y,length=64)
+        string = ""
+        c = Color.NONE
+        for i in range(0,8):
+            x = 7-i
+            string += "{} ".format(x+1)
+            for j in range(0,8):
+                y = 7-j
+                if a[x*8+y]:
+                    c = Color.WHITE
+                elif b[x*8+y]:
+                    c = Color.BLACK
+                else:
+                    c = Color.NONE
+                string += "{} ".format(Color.format(c))
+            string += "\n"
+        string += "  A B C D E F G H"
+        return string
+    def __str__(self):
+        return self.tostring(self.wstate,self.bstate)
 
 
 
@@ -77,16 +200,18 @@ class Game:
         self.initialize_game()
     
     def initialize_game(self):
-        self.game_state = np.zeros((8,8))
-        self.game_state[3][3] = self.game_state[4][4] = Color.black
-        self.game_state[3][4] = self.game_state[4][3] = Color.white
+        self.game_state = Board(0,0)
+        self.game_state[27] = Color.BLACK
+        self.game_state[36] = Color.BLACK
+        self.game_state[28] = Color.WHITE
+        self.game_state[35] = Color.WHITE
 
         self.shallow_depth = 4 #default for shallow depth move ordering
-        self.counter = self.count()
+        self.counter = self.game_state.count()
         self.pruned = 0
         self.evals = 0
         self.moves = 0
-        self.current_player = Color.black
+        self.current_player = Color.BLACK
         self.flipstack = Stack(3000)
     
     structure = np.array([[16, -4, 4, 2, 2, 4, -4, 16],
@@ -96,29 +221,21 @@ class Game:
                             [2, -2, 2, 0, 0, 2, -2, 2],
                             [4, -2, 4, 2, 2, 4, -2, 4],
                             [-4, 12, -2, -2 ,-2, -2, -12,-4],
-                            [16, -4, 4, 2, 2, 4, -4, 16]])
+                            [16, -4, 4, 2, 2, 4, -4, 16]]).flatten()
         
 
     #TODO: Implement actual test
     def move_valid(self, move, player):
-        if self.game_state[move.y][move.x] != Color.none:
+        if self.game_state[move.y*8+move.x] != Color.NONE:
             return False
         return True
 
     def count(self):
-        c = Counter(self.game_state[0])
-        for i in range(1,8):
-            c.update(Counter(self.game_state[i]))
-        return c
+        return self.game_state.count()
        
 
     def draw_board(self):
-        for i in range(0,8):
-            print(8-i, end=" ")
-            for j in range(0,8):
-                print('{}'.format(Color.format_player(self.game_state[i][j])), end=" ")
-            print()
-        print("  A B C D E F G H")
+        print(self.game_state)
 
     def test_move(self, i, j, player):
         retval = []
@@ -135,7 +252,7 @@ class Game:
                     x += xinc
                     y += yinc
                     if x>=0 and y>=0 and x<=7 and y<=7:
-                        if self.game_state[y][x] == Color.ip(player):
+                        if self.game_state[y*8+x] == -player:
                             nmoves += 1
                         else:
                             break
@@ -143,7 +260,7 @@ class Game:
                         nmoves = 0
                         break
 
-                if nmoves >  0 and self.game_state[y][x] == Color.none:
+                if nmoves >  0 and self.game_state[y*8+x] == Color.NONE:
                     if x < 0 or y < 0:
                         print("boink")
                     else:
@@ -155,7 +272,7 @@ class Game:
     def next_moves(self,player):
         next_moves = []
 
-        if self.outside_square() == False:
+        if self.game_state.outside_square() == False:
             rmin = 1
             rmax = 7
         else:
@@ -164,7 +281,7 @@ class Game:
 
         for i in range(rmin,rmax):
             for j in range(rmin,rmax):
-                if(self.game_state[i][j] == player):
+                if(self.game_state[i*8+j] == player):
                     m = self.test_move(i,j,player)
                     next_moves += m
         used = set()
@@ -176,7 +293,7 @@ class Game:
         i = move.y
         j = move.x
 
-        self.game_state[i][j] = player
+        self.game_state[i*8+j] = player
 
         for yinc in range(-1,2):
             for xinc in range(-1,2):
@@ -190,7 +307,7 @@ class Game:
                     x += xinc
                     y += yinc
                     if x>=0 and y>=0 and x<=7 and y<=7:
-                        if self.game_state[y][x] == Color.ip(player):
+                        if self.game_state[y*8+x] == -player:
                             nmoves += 1
                         else:
                             break
@@ -198,17 +315,17 @@ class Game:
                         nmoves = 0
                         break
 
-                if nmoves > 0 and self.game_state[y][x] == player:
+                if nmoves > 0 and self.game_state[y*8+x] == player:
                     x -= xinc
                     y -= yinc
-                    while self.game_state[y][x] == Color.ip(player):
+                    while self.game_state[y*8+x] == -player:
                         self.flipstack.push(y,x)
                         flipped += 1
-                        self.game_state[y][x] = player                      
+                        self.game_state.flip(y*8+x)
                         x -= xinc
                         y -= yinc
         self.counter[player] += flipped + 1
-        self.counter[Color.ip(player)] -= flipped
+        self.counter[-player] -= flipped
 
         self.moves += 1
         return flipped
@@ -217,46 +334,37 @@ class Game:
         i = move.y
         j = move.x
 
-        player = self.game_state[i][j]
+        player = self.game_state[i*8+j]
         self.counter[player] -=  flipped + 1
-        self.counter[Color.ip(player)] += flipped
-        self.game_state[i][j] = Color.none
+        self.counter[-player] += flipped
+        self.game_state[i*8+j] = Color.NONE
 
         for i in range(0,flipped):
             (y,x) = self.flipstack.pop()
-            self.game_state[y][x] = Color.ip(player)
-
-    def outside_square(self):
-        #Trivial case
-        if self.counter[Color.none] >= 48:
-            return True
-        
-        #search square around inner 4x4
-        for i in [1,6]:
-            for j in range(1,6):
-                if self.game_state[i][j] != Color.none:
-                    return True
-
-        for j in [1, 6]:
-            for i in range(2,5):
-                if self.game_state[i][j] != Color.none:
-                    return True
-        #Nothing
-        return False
+            self.game_state.flip(y*8+x)
     
     def eval_structure(self,c):
         structure_sum = 0
-        outside_square = self.outside_square()
+        outside_square = self.game_state.outside_square()
+        mult = 1
 
-        structure_sum = sum(sum(np.matmul(self.game_state,self.structure)))
-
-        #assume outside square
-        if outside_square == False:
-            diff = sum(sum(np.matmul(self.game_state[2:6][2:6],self.structure[2:6][2:6])))
-            structure_sum -= diff
+        if outside_square:
+            b = self.game_state.copy()
+            b.wstate &= ~b.square_mask
+            b.bstate &= ~b.square_mask
+            mult = 3
         else:
-            structure_sum *= 3
-        return c*structure_sum
+            b = self.game_state
+
+        
+        x = btutil.int2ba(b.wstate,length=64)
+        y = btutil.int2ba(b.bstate,length=64)
+        
+        structure_sum = np.matmul(x.tolist(),self.structure)
+        structure_sum -= np.matmul(y.tolist(),self.structure)
+
+        structure_sum *= mult
+        return structure_sum
 
     def eval(self):
         self.evals += 1
@@ -268,7 +376,7 @@ class Game:
         S = self.eval_structure(self.current_player)
         A = self.counter[self.current_player] - self.counter[-self.current_player]
      
-        n = self.counter[Color.white]+self.counter[Color.black]
+        n = self.counter[Color.WHITE]+self.counter[Color.BLACK]
         z = 0.035
         W = m.exp(-n*z)
 
@@ -291,7 +399,7 @@ class Game:
         max_value = -m.inf
         for mov in moves:
             s = self.make_move(c,mov)
-            value = self.minimax_min(Color.ip(c),depth-1)
+            value = self.minimax_min(-c,depth-1)
             max_value = max(max_value,value)
             self.undo_move(mov,s)
         return (value,retmv)
@@ -306,7 +414,7 @@ class Game:
         min_value = m.inf
         for mov in moves:
             s = self.make_move(c,mov)
-            (value,xxx) = self.minimax_max(Color.ip(c),depth-1)
+            (value,xxx) = self.minimax_max(-c,depth-1)
             min_value = min(min_value, value)
             self.undo_move(mov,s)
         return value
@@ -321,6 +429,7 @@ class Game:
         alpha = -m.inf
         beta = m.inf
         max_value = alpha
+        value = 0
         for mov in moves:
             s = self.make_move(self.current_player,mov)
             value = self.alphabeta_min(self.shallow_depth-1,max_value,beta)
@@ -363,7 +472,7 @@ class Game:
         return max_value
     
     def alphabeta_min(self, depth, alpha, beta):
-        moves = self.next_moves(Color.ip(self.current_player))
+        moves = self.next_moves(-self.current_player)
         if len(moves) == 0 or depth == 0:
             return self.eval()
 
@@ -378,9 +487,75 @@ class Game:
                     self.pruned += 1
                     break
         return min_value
+
+
+        #check y symmetry:
+#        a = self.flip_vertically(self.wstate)
+#        b = self.flip_vertically(self.wstate)
+#        if a == self.wstate and b == self.bstate:
+#            symmetry |= 2
+        #check symmetry along (08) to (80)
+#        a = self.bit_reverse_64(self.wstate)
+#        b = self.bit_reverse_64(self.bstate)
+#        if a == self.wstate and b == self.bstate:
+#            symmetry |= 8
+#        return symmetry
+def test():
+    g = Game()
+    b = g.game_state
+    b[0] = Color.BLACK
+    b[7] = Color.BLACK
+    b[63] = Color.BLACK
+    b[62] = Color.WHITE
+    b[56] = Color.WHITE
+    b = g.game_state.copy()
+    print("x symmetry")
+    print(b)
+    b.wstate = b.bit_reverse_8(b.wstate)
+    b.bstate = b.bit_reverse_8(b.bstate)
+    print(b)
+    b = g.game_state.copy()
+    print("y symmetry")
+    print (b)
+    b.wstate = b.flip_vertically(b.wstate)
+    b.bstate = b.flip_vertically(b.bstate)
+    print(b)
+    b = g.game_state.copy()
+    print("180 degree rotation")
+    print (b)
+    b.wstate = b.bit_reverse_64(b.wstate)
+    b.bstate = b.bit_reverse_64(b.bstate)
+    print(b)
+    b = g.game_state.copy()
+    print("00 88 symmetry")
+    print (b)
+    b.wstate = b.bit_reverse_8(b.wstate)
+    b.bstate = b.bit_reverse_8(b.bstate)
+    b.wstate = b.flip_vertically(b.wstate)
+    b.bstate = b.flip_vertically(b.bstate)  
+
+    print(b)
+
+
+
+        
     
 def main():
+    #run()
+    test()
+    
+
+
+def run():
     g = Game()
+    b = g.game_state
+    b[2*8+5] = Color.WHITE
+    b[3*8+5] = Color.BLACK
+    b[4*8+2] = Color.BLACK
+    b[5*8+2] = Color.WHITE
+    print(b)
+
+    g.shallow_depth = 4
 
     passing = 0
     moves = []
@@ -389,25 +564,14 @@ def main():
 
     pm = None
 
-    g.game_state[2][5] = Color.white
-    g.game_state[3][5] = Color.black
-    g.game_state[4][2] = Color.black
-    g.game_state[5][2] = Color.white
-    g.counter = g.count()
-
-    g.draw_board()
-
-
-    g.shallow_depth = 4
-
 
     while True:
-        #if cp == Color.black:
+        #if cp == Color.BLACK:
         cev = g.evals
         cmv = g.moves
         cpr = g.pruned
         t1 = time.time()
-        (value,mv) = g.alphabeta_init(12)
+        (value,mv) = g.alphabeta_init(7)
         #(value,mv) = g.minimax_max(cp,g.depth)
         t1 = time.time() - t1
         print("Time: {}s".format(t1))
@@ -432,21 +596,21 @@ def main():
             g.make_move(g.current_player,mv)
             moves.append(mv)
             g.draw_board()
-            print("Color: {} Move: {} Value: {:0.9f}".format(Color.format_player(g.current_player),mv,value))
+            print("Color: {} Move: {} Value: {:0.9f}".format(Color.format(g.current_player),mv,value))
             passing = 0
         else:
             passing += 1
-            print("Color: {}".format(Color.format_player(g.current_player)))
+            print("Color: {}".format(Color.format(g.current_player)))
         if passing == 2:
             break
 
         g.current_player = -g.current_player
     
     c = g.count()
-    if c[Color.white] > c[Color.black]:
-        print("White won!")
+    if c[Color.WHITE] > c[Color.BLACK]:
+        print("WHITE won!")
     else:
-        print("Black won!")
+        print("BLACK won!")
 
     for mov in moves:
         print(mov,end="")
